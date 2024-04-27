0c8b793a8642b4175f703216c2ee58d7af2f7369
Dermot Treanor <dermot.treanor1@gmail.com>
2024-04-27 20:38:50 +0100

    created flag, and loop to read csv data

--- main.go
+++ main.go
@@ -1,10 +1,38 @@
 package main
 
-import(
+import (
+	"encoding/csv"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
+var filename *string = flag.String("csv", "problem.csv", "this is the help message for this flag")
+
 func main(){
-	fmt.Fprint(os.Stdout, "Hello world!\n")
+	flag.Parse()
+	file, err := os.Open(*filename)
+	if err != nil{
+		fmt.Printf("The following error occured: %v\n", err)
+	}	
+	
+	//Since os.File is a type that implements io.Reader we can pass it here.
+	//This basically means it is a type that the NewReader can read data from to make its own Reader.
+	var csv_reader *csv.Reader = csv.NewReader(file)
+	var csv_line []string
+	for true{
+		csv_line, err = csv_reader.Read()
+		if err == io.EOF{
+			fmt.Println("That's the end of the quiz!")
+			break
+		}else if err != nil{
+			fmt.Printf("There is an UNEXPECTED error: %v\n", err)
+		}
+		fmt.Println("What is " + csv_line[0]+ ": ")
+		var answer string = "%s"
+		fmt.Scanf(answer)
+		fmt.Println(answer)
+	}
+	
 }
