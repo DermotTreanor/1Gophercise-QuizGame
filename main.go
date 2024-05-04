bd29d7c591253793e794049240b9bd5ad4fce426
Dermot Treanor <dermot.treanor1@gmail.com>
2024-05-04 20:57:05 +0100

    fixed input scanning, added function for basic regex

--- main.go
+++ main.go
@@ -6,33 +6,49 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 )
+
+func get_math_expression(question string) string{
+	reg, err := regexp.Compile("[0-9]+[\\+\\-\\*/][0-9]+")
+	if err != nil{
+		fmt.Println(err)
+	}
+	findings := reg.Find([]byte(question))
+	return string(findings)
+}
 
 var filename *string = flag.String("csv", "problem.csv", "this is the help message for this flag")
 
 func main(){
 	flag.Parse()
+
 	file, err := os.Open(*filename)
 	if err != nil{
 		fmt.Printf("The following error occured: %v\n", err)
 	}	
 	
 	//Since os.File is a type that implements io.Reader we can pass it here.
-	//This basically means it is a type that the NewReader can read data from to make its own Reader.
 	var csv_reader *csv.Reader = csv.NewReader(file)
 	var csv_line []string
-	for true{
+	var total_questions, right_ans, wrong_ans int 
+	for{
 		csv_line, err = csv_reader.Read()
 		if err == io.EOF{
 			fmt.Println("That's the end of the quiz!")
 			break
 		}else if err != nil{
 			fmt.Printf("There is an UNEXPECTED error: %v\n", err)
+			continue
 		}
-		fmt.Println("What is " + csv_line[0]+ ": ")
-		var answer string = "%s"
-		fmt.Scanf(answer)
-		fmt.Println(answer)
+		total_questions++
+		fmt.Println(get_math_expression(csv_line[0]))
+		
+		var given_input string
+		fmt.Scanf("%s", &given_input)
+		fmt.Println(given_input)
 	}
+
+	fmt.Println(total_questions, right_ans, wrong_ans)
 	
 }
